485bbea1053666edce1f4863ca9a9ce492d3c771
= <=>
2024-09-09 13:27:49 +0530

    application complete

---
+++ app/chains.py
@@ -0,0 +1,56 @@
+import os
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.exceptions import OutputParserException
+from dotenv import load_dotenv 
+
+load_dotenv()
+os.getenv("GROQ_API_KEY")
+
+class Chain:
+    def __init__(self):
+        self.llm = ChatGroq(temperature=0,
+                            groq_api_key=os.getenv("GROQ_API_KEY"),
+                            model_name="llama-3.1-70b-versatile")
+
+    def extract_jobs(self, cleaned_text):
+        prompt_extract = PromptTemplate.from_template(
+            """
+            ### SCRAPED TEXT FROM WEBSITE:
+            {page_data}
+            ### INSTRUCTION:
+            The scraped text is from the career's page of a website.
+            Your job is to extract the job postings and return them in JSON format containing the following keys: `role`, `experience`, `skills` and `description`.
+            Only return the valid JSON.
+            ### VALID JSON (NO PREAMBLE):
+            """
+        )
+        chain_extract = prompt_extract | self.llm
+        res = chain_extract.invoke(input={"page_data": cleaned_text})
+        try:
+            json_parser = JsonOutputParser()
+            res = json_parser.parse(res.content)
+        except OutputParserException:
+            raise OutputParserException("Context too big. Unable to parse jobs.")
+        return res if isinstance(res, list) else [res]
+
+    def write_mail(self, job, links):
+        prompt_email = PromptTemplate.from_template(
+            """
+            ### JOB DESCRIPTION:
+            {job_description}
+
+            ### INSTRUCTION:
+            You are Soham Chaudhari, a B.E Computer Engineering student with a strong passion for web development and a growing interest in data science and machine learning. You have developed dynamic web applications and innovative solutions at the intersection of technology and practical problem-solving. Your goal is to contribute to impactful projects and drive advancements in these evolving fields.
+            Write a cold email to the recruiter regarding the job mentioned above, describing how your background and skills make you a strong fit for the position. Reference your relevant projects and skills from your resume to showcase your capabilities.
+            ### EMAIL (NO PREAMBLE):
+
+            """
+        )
+        chain_email = prompt_email | self.llm
+        res = chain_email.invoke({"job_description": str(job), "link_list": links})
+        return res.content
+
+if __name__ == "__main__":
+    print(os.getenv("GROQ_API_KEY"))
